f297a7bd96a8bfbe537929c71a98208052587e0f
Yuta Takahashi <b1015143@fun.ac.jp>
2018-05-24 15:41:43 +0900

    [delete] generate

--- src/states/TodoState.ts
+++ src/states/TodoState.ts
@@ -5,21 +5,3 @@ export interface ITodo {
 }
 
 export type Todos = ITodo[];
-
-let id: number = 0;
-
-function generateTodo(text: string, id: number): ITodo {
-    return {
-        completed: false,
-        id: id,
-        text: text
-    }
-}
-
-export function generateTodos(text: string[]): ITodo[] {
-    return text.map(t => {
-        let res = generateTodo(t, id);
-        id++;
-        return res;
-    })
-}
